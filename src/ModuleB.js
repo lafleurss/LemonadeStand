5b5cbdcf8c701f2a1ce8ad4844dad0d5fb0a9c19
Shilpa Nair <101418016+lafleurss@users.noreply.github.com>
2023-04-05 17:44:48 -0500

    init

---
+++ src/ModuleB.js
@@ -0,0 +1,11 @@
+require('./moduleA')
+
+let varB = 'Module B'
+
+console.log('Module B');
+
+console.log('Module B More code');
+
+console.log('Done with Module B');
+
+console.log(varA)
---
+++ src/moduleA.js
@@ -0,0 +1,7 @@
+console.log('Module A');
+
+console.log('Module A More code');
+
+console.log('Done with Module A');
+
+let varA = "Module A"
